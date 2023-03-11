365bcc27904bc3fb561437da6039f8dd0d8a3b49
lavrentev <loktarmp@gmail.com>
2023-03-11 11:49:37 +0300

    method is_present now from class BasePage, so it available for LoginPage objects, also locators for LoginPage completed

--- pages/base_page.py
+++ pages/base_page.py
@@ -9,3 +9,11 @@ class BasePage:
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
--- pages/locators.py
+++ pages/locators.py
@@ -2,10 +2,14 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK = (By.XPATH, '//a[@id="login_link"]')
 
 
 class LoginPageLocators():
-    EMAIL_FORM = ''
-    PASS_FORM = ''
-    SUBMIT_BUTTON = ''
+    LOGIN_EMAIL_FORM = (By.XPATH, '//input[@id="id_login-username"]')
+    LOGIN_PASS_FORM = (By.XPATH, '//input[@id="id_login-password"]')
+    LOGIN_SUBMIT_BUTTON = (By.XPATH, '//button[@name="login_submit"]')
+    REGISTER_EMAIL_FORM = (By.XPATH, '//input[@id="id_registration-email"]')
+    REGISTER_PASS1_FORM = (By.XPATH, '//input[@id="id_registration-password1"]')
+    REGISTER_PASS2_FORM = (By.XPATH, '//input[@id="id_registration-password2"]')
+    REGISTER_SUBMIT_BUTTON = (By.XPATH, '//button[@name="registration_submit"]')
