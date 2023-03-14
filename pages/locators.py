46dc9df513871bc86428c1ad32315eabc8a3f1d3
lavrentev <loktarmp@gmail.com>
2023-03-14 15:36:45 +0300

    Add locators for product_page

--- pages/locators.py
+++ pages/locators.py
@@ -13,3 +13,13 @@ class LoginPageLocators():
     REGISTER_PASS1_FORM = (By.XPATH, '//input[@id="id_registration-password1"]')
     REGISTER_PASS2_FORM = (By.XPATH, '//input[@id="id_registration-password2"]')
     REGISTER_SUBMIT_BUTTON = (By.XPATH, '//button[@name="registration_submit"]')
+
+
+class ProductPageLocators():
+    BOOK_TITLE = (By.XPATH, '//div[contains(@class,"product_main")]/h1')
+    BOOK_PRICE = (By.XPATH, '//p[@class="price_color"]')
+    ADD_TO_BASKET_BUTTON = (By.XPATH, '//form[@id="add_to_basket_form"]/button')
+    BOOK_ADDED_TITLE_ALERT = (By.XPATH, '//div[@id="messages"]/strong')
+    BOOK_ADDED_PRICE_ALERT = (By.XPATH, '//div[@id="messages"]/p/strong')
+
+
