14d65c5128157beadc6400d08d98821fa5be4be9
DefaultUser_2.0 <lawrencemp@yandex.ru>
2023-03-17 14:34:43 +0300

    add parametrized tests for promos, and 1 test mark as fail

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,26 @@
+import pytest
 from .pages.product_page import ProductPage
+from .utilities.urls import UrlsTest
 
 
-def test_guest_can_add_product_to_basket(browser):
-    product_link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    product_page = ProductPage(browser, product_link, 10)
+@pytest.mark.parametrize('link', [
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                 marks=pytest.mark.xfail),
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+])
+def test_guest_can_add_product_to_basket(browser, link):
+ #   product_link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+    product_page = ProductPage(browser, link, 10)
     product_page.open()
     product_page.check_adding_to_basket()
+
 
